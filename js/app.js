7246112d0f79964874e170d8ef7b4ace8ffdcfd4
Ronaldo Torres <rtorres66@misena.edu.co>
2022-05-31 09:31:23 -0500

    Creando manifest.json, sw y validado si el navegador soporta Sw

---
+++ js/app.js
@@ -0,0 +1,11 @@
+//Registrar el Service Worker
+
+if( 'serviceWorker' in navigator ) {
+
+    navigator.serviceWorker.register('./sw.js')
+        .then( registrado => console.log('Se instalo correctamente', registrado))
+        .catch( error => console.log('Fallo la instalación', error))
+
+} else {
+    console.log('Service Workers No Soportado!');
+}
