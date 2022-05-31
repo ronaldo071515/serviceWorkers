845ebb60b989c0bf71ed876bfadc2e06130778a4
Ronaldo Torres <rtorres66@misena.edu.co>
2022-05-31 10:51:41 -0500

    Cacheando los archivos principales

--- sw.js
+++ sw.js
@@ -1,14 +1,37 @@
+const nombreCache = 'apv-v1';
+
+const archivos = [
+    "/",
+    "index.html",
+    "./css/bootstrap.css",
+    "./css/styles.css",
+    "./js/app.js",
+    "./js/apv.js",
+  ];
+
 
 //Cuando se instala el service worker
 self.addEventListener('install', e => {
     console.log('Instalado el service worker');
 
-    console.log(e);
+    e.waitUntil(
+        caches.open(nombreCache)
+            .then( cache => {
+                console.log('Cacheando');
+                cache.addAll(archivos);
+            })
+    )
+
 });
 
 //Activar el Service Worker
 self.addEventListener('activate', e => {
     console.log('Service Worker Acticado');
 
-    console.log(e);
+    //console.log(e);
+});
+
+//Evento fetch para descargar archivos estaticos
+self.addEventListener('fetch', e => {
+    //console.log('Fetch....', e);
 });
